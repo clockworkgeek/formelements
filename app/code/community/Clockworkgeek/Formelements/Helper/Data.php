9421cf702538dc687e5e75d93177b22e2127b6e8
Daniel Deady <daniel@clockworkgeek.com>
2015-05-15 18:39:40 +0100

    Remove unused methods

--- app/code/community/Clockworkgeek/Formelements/Helper/Data.php
+++ app/code/community/Clockworkgeek/Formelements/Helper/Data.php
@@ -25,14 +25,4 @@
 
 class Clockworkgeek_Formelements_Helper_Data extends Mage_Core_Helper_Data
 {
-
-    public function convertToDir($data)
-    {
-        return Mage::getSingleton('formelements/template_localdir')->filter($data);
-    }
-
-    public function convertToUrl($data)
-    {
-        return Mage::getSingleton('widget/template_filter')->filter($data);
-    }
 }
